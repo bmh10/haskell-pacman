3e15ffcbfd867af8015752e0cc41aa160d6029e6
bmh10 <bmhomer13@gmail.com>
2016-10-09 19:25:09 +0100

    Moving pacman

--- Main.hs
+++ Main.hs
@@ -2,7 +2,7 @@ module Main where
 
 import Graphics.Gloss
 import Graphics.Gloss.Data.ViewPort
-import Graphics.Gloss.Interface.IO.Game
+import Graphics.Gloss.Interface.Pure.Game
 import System.IO  
 import Control.Monad
 
@@ -21,15 +21,20 @@ type Position = (Float, Float)
 
 data PacmanGame = Game
   { 
-    level :: [String]
+    level :: [String],
+    pacmanPos :: Position
   } deriving Show 
 
-render :: PacmanGame -> IO Picture 
-render game
-  | otherwise       = renderGame game
+render :: PacmanGame -> Picture 
+render g = pictures [renderPacman g, renderLevel g]
 
-renderGame :: PacmanGame -> IO Picture
-renderGame game = return $ renderLines (level game) (snd topLeft)
+renderPacman :: PacmanGame -> Picture 
+renderPacman game = translate x y $ color orange $ circleSolid (tileSize/2-1)
+  where 
+    (x, y) = pacmanPos game
+
+renderLevel :: PacmanGame -> Picture
+renderLevel game = renderLines (level game) (snd topLeft)
 
 renderLines :: [String] -> Float -> Picture
 renderLines [] _ = blank
@@ -44,26 +49,36 @@ renderTile 'x' x y = translate x y $ color blue $ rectangleSolid (tileSize-1) (t
 renderTile '+' x y = translate x y $ color white $ rectangleSolid (tileSize-1) 2
 renderTile '.' x y = translate x y $ color yellow $ circleSolid 2
 renderTile 'o' x y = translate x y $ color yellow $ circleSolid 4
+renderTile 'p' x y = translate x y $ color orange $ circleSolid (tileSize/2-1)
 renderTile _ _ _ = blank
 
 -- Event handling
-handleKeys :: Event -> PacmanGame -> IO PacmanGame
-handleKeys (EventKey (Char 'r') Down _ _) game = return game
-handleKeys _ game = return game
+handleKeys :: Event -> PacmanGame -> PacmanGame
+handleKeys (EventKey (Char 'd') Down _ _) game = move game 1 0
+handleKeys (EventKey (Char 'a') Down _ _) game = move game (-1) 0
+handleKeys (EventKey (Char 'w') Down _ _) game = move game 0 1
+handleKeys (EventKey (Char 's') Down _ _) game = move game 0 (-1)
+handleKeys _ game = game
 
-update :: Float -> PacmanGame -> IO PacmanGame
-update seconds game = return game
+move game xm ym = game {pacmanPos = (x', y')}
+  where
+    (x, y) = pacmanPos game
+    x' = x + xm*tileSize
+    y' = y + ym*tileSize
+
+update :: Float -> PacmanGame -> PacmanGame
+update seconds game = game
 
 -- Not sure why print is required...
 initTiles = do 
   handle <- openFile "2.lvl" ReadMode
   contents <- hGetContents handle
   let rows = words contents
-  let initialState = Game { level = rows }
+  let initialState = Game { level = rows, pacmanPos = (0, 0) }
   print rows
   hClose handle
   return initialState
 
 main = do
   initialState <- initTiles
-  playIO window background fps initialState render handleKeys update
+  play window background fps initialState render handleKeys update
