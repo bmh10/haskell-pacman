6ec0315a12e42c7acd4edf0153b0da3001380cce
bmh10 <bmhomer13@gmail.com>
2016-10-09 20:53:26 +0100

    Add tileCoord to screenCoord function. Pacman can't go through walls anymore

--- Main.hs
+++ Main.hs
@@ -7,13 +7,11 @@ import System.IO
 import Control.Monad
 
 fps = 60
-topLeft = (-fromIntegral width/2 + tileSize/2, fromIntegral height/2 - tileSize/2)
 width = 420 -- 28 * 15
 height = 465 -- 31 * 15
 offset = 100
-tilesHorizontal = 30
 tileSize = 15
-pacmanInitialPos = (tileSize/2, -8*tileSize)
+pacmanInitialPos = (1,1)
 window = InWindow "Pacman" (width, height) (offset, offset)
 background = black
 
@@ -22,50 +20,61 @@ type Position = (Float, Float)
 
 data PacmanGame = Game
   { 
-    level :: [String],
-    pacmanPos :: Position
+    level :: [String],      -- Level layout
+    pacmanPos :: (Int, Int) -- Tile coord of pacman
   } deriving Show 
 
+-- Tile functions
+getTile :: PacmanGame -> Int -> Int -> Char
+getTile g x y = (level g) !! y !! x
+
+-- Map tile coords ((0,0) is top-left tile) to actual screen coords ((0, 0) is center of screen)
+tileToCoord :: (Int, Int) -> Position 
+tileToCoord (x, y) = (fromIntegral x*tileSize + tileSize/2 - fromIntegral width/2, fromIntegral height/2 - fromIntegral y*tileSize - tileSize/2)
+
+-- Rendering
 render :: PacmanGame -> Picture 
-render g = pictures [renderPacman g, renderLevel g]
+render g = pictures [renderLevel g, renderPacman g]
 
 renderPacman :: PacmanGame -> Picture 
 renderPacman game = translate x y $ color orange $ circleSolid (tileSize/2-1)
   where 
-    (x, y) = pacmanPos game
+    (x, y) = tileToCoord $ pacmanPos game
 
 renderLevel :: PacmanGame -> Picture
-renderLevel game = renderLines (level game) (snd topLeft)
+renderLevel game = renderLines (level game) 0
 
-renderLines :: [String] -> Float -> Picture
+renderLines :: [String] -> Int -> Picture
 renderLines [] _ = blank
-renderLines (l:ls) y = pictures [renderLine l (fst topLeft) y, renderLines ls (y-tileSize)]
+renderLines (l:ls) y = pictures [renderLine l 0 y, renderLines ls (y+1)]
 
-renderLine :: String -> Float -> Float -> Picture
+renderLine :: String -> Int -> Int -> Picture
 renderLine [] _ _      = blank
-renderLine (t:ts) x y  = pictures [renderTile t x y, renderLine ts (x+tileSize) y]
+renderLine (t:ts) x y  = pictures [renderTile t x y, renderLine ts (x+1) y]
 
-renderTile :: Char -> Float -> Float -> Picture
-renderTile 'x' x y = translate x y $ color blue $ rectangleSolid (tileSize-1) (tileSize-1)
-renderTile '+' x y = translate x y $ color white $ rectangleSolid (tileSize-1) 2
-renderTile '.' x y = translate x y $ color yellow $ circleSolid 2
-renderTile 'o' x y = translate x y $ color yellow $ circleSolid 4
---renderTile 'p' x y = translate x y $ color orange $ circleSolid (tileSize/2-1)
-renderTile _ _ _ = blank
+renderTile :: Char -> Int -> Int -> Picture
+renderTile c x y
+ | c == 'x'  = translate x' y' $ color blue $ rectangleSolid (tileSize-1) (tileSize-1)
+ | c == '+'  = translate x' y' $ color white $ rectangleSolid (tileSize-1) 2
+ | c == '.'  = translate x' y' $ color yellow $ circleSolid 2
+ | c == 'o'  = translate x' y' $ color yellow $ circleSolid 4
+ | otherwise = blank
+  where
+    (x', y') = tileToCoord (x, y)
 
 -- Event handling
 handleKeys :: Event -> PacmanGame -> PacmanGame
 handleKeys (EventKey (Char 'd') Down _ _) game = move game 1 0
 handleKeys (EventKey (Char 'a') Down _ _) game = move game (-1) 0
-handleKeys (EventKey (Char 'w') Down _ _) game = move game 0 1
-handleKeys (EventKey (Char 's') Down _ _) game = move game 0 (-1)
+handleKeys (EventKey (Char 'w') Down _ _) game = move game 0 (-1)
+handleKeys (EventKey (Char 's') Down _ _) game = move game 0 1
 handleKeys _ game = game
 
 move game xm ym = game {pacmanPos = (x', y')}
   where
     (x, y) = pacmanPos game
-    x' = x + xm*tileSize
-    y' = y + ym*tileSize
+    x' = if getTile game (x+xm) y == 'x' then x else x + xm
+    y' = if getTile game x (y+ym) == 'x' then y else y + ym
 
 update :: Float -> PacmanGame -> PacmanGame
 update seconds game = game
