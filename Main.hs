1230ebdf1d8b422ca3228d8db13528adefe1f14d
bmh10 <bmhomer13@gmail.com>
2016-10-09 12:51:54 +0100

    Render tiles in lines

--- Main.hs
+++ Main.hs
@@ -10,6 +10,7 @@ fps = 60
 width = 600
 height = 400
 offset = 100
+tileSize = 30
 window = InWindow "Pacman" (width, height) (offset, offset)
 background = black
 
@@ -24,7 +25,7 @@ data PacmanGame = Game
 initialState :: PacmanGame
 initialState = Game
   { 
-    level = ["0000", "1111"]
+    level = ["11111", "01110", "11111"]
   }
 
 render :: PacmanGame -> IO Picture 
@@ -32,11 +33,19 @@ render game
   | otherwise       = renderGame game
 
 renderGame :: PacmanGame -> IO Picture
-renderGame game = return $ f ("101011") 0
+renderGame game = return $ renderLines (level initialState) 0
 
-f :: String -> Float -> Picture
-f [] _       = blank
-f (x:xs) xco = translate [pictures xco 0 $ color (greyN 0.5) $ rectangleSolid 30 30, f xs (xco+40)]
+renderLines :: [String] -> Float -> Picture
+renderLines [] _ = blank
+renderLines (l:ls) y = pictures [renderLine l 0 y, renderLines ls (y-tileSize)]
+
+renderLine :: String -> Float -> Float -> Picture
+renderLine [] _ _      = blank
+renderLine (t:ts) x y  = pictures [renderTile t x y, renderLine ts (x+tileSize) y]
+
+renderTile :: Char -> Float -> Float -> Picture
+renderTile '0' _ _ = blank
+renderTile '1' x y = translate x y $ color red $ rectangleSolid (tileSize-1) (tileSize-1)
 
 -- Event handling
 handleKeys :: Event -> PacmanGame -> IO PacmanGame
@@ -44,13 +53,16 @@ handleKeys (EventKey (Char 'r') Down _ _) game = return game
 handleKeys _ game = return game
 
 update :: Float -> PacmanGame -> IO PacmanGame
-update seconds game = do  
+update seconds game = return game
+
+initTiles = do 
   handle <- openFile "1.lvl" ReadMode
   contents <- hGetContents handle
   let singlewords = words contents
-  --game {level = singlewords}
+  return $ initialState {level = ["00100", "01110", "11111"]}
   print singlewords
   hClose handle
-  return (game)
 
-main = playIO window background fps initialState render handleKeys update
+main = do
+  initTiles
+  playIO window background fps initialState render handleKeys update
